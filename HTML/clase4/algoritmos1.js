8121fc11e8b2edcc0634dc39ffc86e8eff34bbe5
unknown <paula.schulz.01@gmail.com>
2020-11-04 20:10:00 -0300

    mi primer comentario

---
+++ HTML/clase4/algoritmos1.js
@@ -0,0 +1,19 @@
+//for
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+console.log(i) = 0, 1, 2, 3, 4
+
+
+for (var i = 0; i < 5; i++) {
+    i = i + 1;
+    console.log(i); //1,3,5
+}
+//i=0>1>2>3>4>5>6
+
+console.log("hola clase");
+console.log(4);
+console.log(true);
+var x = 3;
+console.log(x);
