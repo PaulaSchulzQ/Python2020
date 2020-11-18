32a239830c8528c40c74fee06e9c18dbea15efcd
Your Name <paula.schulz.01@gmail.com>
2020-11-18 20:59:17 -0300

    agrego carpeta jasmine

--- JAVASCRIPT/intermedio.js
+++ JAVASCRIPT/intermedio.js
@@ -30,8 +30,18 @@ a = factorial(5);
 //Fibonacci - 
 //Crea una función para generar números de Fibonacci. 
 //En esta famosa secuencia matemática, cada número es la suma de las dos anteriores, 
-//partiendo con los valores 0 y 1. Tu función debería aceptar un argumento, un índice en la secuencia (donde 0 corresponde al valor inicial, 4 corresponden al valor cuatro más tarde, etc). Ejemplos: fibonacci(0) = 0 (dado), fibonacci(1) = 1 (dado), fibonacci(2) = 1 (fib(0)+fib(1), o 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, o 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8). Haz esto primero sin usar recursión. Si no sabes qué es una recursión, no te preocupes puesto que vamos a introducir este concepto en la Parte 2 de esta actividad. 
-
+//partiendo con los valores 0 y 1. 
+//Tu función debería aceptar un argumento, un índice en la secuencia 
+//(donde 0 corresponde al valor inicial, 4 corresponden al valor cuatro más tarde, etc).
+// Ejemplos: fibonacci(0) = 0 (dado), fibonacci(1) = 1 (dado), fibonacci(2) = 1 (fib(0)+fib(1), o 0+1),
+// fibonacci(3) = 2 (fib(1) + fib(2)3, o 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), 
+//fibonacci(7) = 13 (5+8). 
+//Haz esto primero sin usar recursión. 
+//Si no sabes qué es una recursión, no te preocupes puesto que vamos a introducir este concepto en la Parte 2 de esta actividad. 
+function fibonacci(x, y) {
+    var temp = 0;
+    temp =
+}
 
 
 //Array: Penúltimo: 
