5a80d768a03721966bf9aeddf91c1decf1590a8c
Your Name <paula.schulz.01@gmail.com>
2021-01-19 23:20:42 -0300

    subiendo tarea intermedios 2

--- python_stack/python/fundamentals/funciones_intermedias2.py
+++ python_stack/python/fundamentals/funciones_intermedias2.py
@@ -61,6 +61,53 @@ print(iterateDictionary(students))
 #Rosales
 #Guillen
 #Tonel
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+def iterateDictionary2(key_name, some_list)
+    for i in some_list:
+        print(i[key_name])
+
+iterateDictionary2("first_name", students)
+iterateDictionary2("last_name", students)
 
 
+#4.- Itera a través de un diccionario con valores de listas
+#Crea una función printInfo(some_dict)que, dado un diccionario cuyos valores son todas listas, imprima el nombre 
+#de cada clave junto con el tamaño de su lista, y luego imprima los valores asociados dentro de la lista de cada clave.
 
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+#printInfo(dojo)
+# output:
+#7 LOCATIONS
+#San Jose
+#Seattle
+#Dallas
+#Chicago
+#Tulsa
+#DC
+#Burbank
+    
+#8 INSTRUCTORS
+#Michael
+#Amy
+#Eduardo
+#Josh
+#Graham
+#Patrick
+#Minh
+#Devon
+
+def printInfo(some_dict):
+    for key in some_dict:
+        print(len(some_dict[key]), key.upper())
+        for each_list_value in some_dict[key]:
+            print(each_list_value)
+        print("")
+
+print(printInfo(dojo))
--- python_stack/python/fundamentals/prueba.py
+++ python_stack/python/fundamentals/prueba.py
@@ -1,102 +1,33 @@
-def tamanio_grande(arr):
-    contador = 0
-    for i in arr:
-        if i > 0:
-            arr[contador]="Big"
-        contador = contador + 1
-    return arr
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+#printInfo(dojo)
+# output:
+#7 LOCATIONS
+#San Jose
+#Seattle
+#Dallas
+#Chicago
+#Tulsa
+#DC
+#Burbank
+    
+#8 INSTRUCTORS
+#Michael
+#Amy
+#Eduardo
+#Josh
+#Graham
+#Patrick
+#Minh
+#Devon
 
-print(tamanio_grande([-3,2,1,-5]))
+def printInfo(some_dict):
+    for key in some_dict:
+        print(len(some_dict[key]), key.upper())
+        for each_list_value in some_dict[key]:
+            print(each_list_value)
+        print("")
 
-
-#Contar positivos : dada una lista de números, cree una función para reemplazar el último valor con el número de valores positivos.
-# (Tenga en cuenta que cero no se considera un número positivo).
-#Ejemplo: count_positives ([- 1,1,1,1]) cambia la lista original a [-1,1,1,3] y la devuelve
-#Ejemplo: count_positives ([1,6, -4, -2, -7, -2]) cambia la lista a [1,6, -4, -2, -7,2] y la devuelve
-def contar_positivos(arr):
-    nuevoarr=[]
-    for i in arr:
-        if i>0:
-            nuevoarr.append(i)
-            arr[len(arr)-1]=len(nuevoarr)
-    return arr
-
-print(contar_positivos([-1,1,1,1]))
-
-
-#Suma total : crea una función que toma una lista y devuelve la suma de todos los valores de la matriz.
-#Ejemplo: sum_total ([1,2,3,4]) debería devolver 10
-#Ejemplo: sum_total ([6,3, -2]) debería devolver 7
-def suma_total(arr):
-    var=(0)
-    for i in arr:
-        var+=i
-    return var
-
-print(suma_total([1,2,3,4]))
-
-
-#Promedio : crea una función que toma una lista y devuelve el promedio de todos los valores.
-#Ejemplo: el promedio ([1,2,3,4]) debería devolver 2.5
-def promedio(arr):
-    suma=0
-    for i in arr:
-        suma+=i
-    return suma/len(arr)
-
-print(promedio([1,2,3,4]))
-
-#Longitud : crea una función que toma una lista y devuelve la longitud de la lista.
-#Ejemplo: la longitud ([37,2,1, -9]) debería devolver 4
-#Ejemplo: longitud ([]) debería devolver 0
-def longitud(arr):
-    return len(arr)
-
-print(longitud([37,2,3,4,-1]))
-
-#Mínimo : crea una función que tome una lista de números y devuelva el valor mínimo en la lista. Si la lista está vacía, haga que la función devuelva False.
-#Ejemplo: mínimo ([37,2,1, -9]) debería devolver -9
-#Ejemplo: mínimo ([]) debería devolver False
-def minimo(arr):
-    if len(arr)==0:
-        return False
-    val_min=arr[0]
-    for i in arr:
-        if i<=val_min:
-            val_min=i
-            if i>val_min:
-                val_min=val_min
-    return val_min
-
-print(minimo([37,1,2,3,-9]))
-
-#Máximo : crea una función que toma una lista y devuelve el valor máximo en la matriz. Si la lista está vacía, haga que la función devuelva False.
-#Ejemplo: máximo ([37,2,1, -9]) debería devolver 37
-#Ejemplo: máximo ([]) debería devolver False
-def maximo(arr):
-    if len(arr)==0:
-        return False
-    val_max=arr[0]
-    for i in arr:
-        if i>=val_max:
-            val_max=i
-            if i<val_max:
-                val_max=val_max
-    return val_max
-
-print(maximo([37,1,2,3,-9]))
-
-#Análisis final : crea una función que tome una lista y devuelva un diccionario que tenga la 
-#suma total, promedio, mínimo, máximo y longitud de la lista.
-#Ejemplo: ultimate_analysis ([37,2,1, -9]) debería devolver {'totalTotal': 31, 'promedio': 7.75, 'mínimo': -9, 'máximo': 37, 'longitud': 4}
-def final(arr):
-    diccionario={
-        "suma":suma_total(arr),
-        "promedio":promedio(arr),
-        "minimo":minimo(arr),
-        "maximo":maximo(arr),
-        "longitud":len(arr)
-    }
-    return diccionario
-
-print(final([2,3,5,7,4,88,3,-4]))
+print(printInfo(dojo))
